f42e4cbea1818d4fda125c87b1fc0c8b50b7c51f
tawshif <anikphp@gmail.com>
2017-03-13 00:26:52 +0600

    currently to tuts-52

---
+++ app/fello.js
@@ -0,0 +1,5 @@
+const fello = ['abc','abc2','abc3'];
+const total = fello.length;
+
+
+export {total};
---
+++ app/index.js
@@ -0,0 +1,7 @@
+import {fello, total} from './fello.js';
+import multiply  from './math.js';
+
+
+console.log(fello, total);
+
+console.log(multiply(5,6));
