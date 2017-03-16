4de153fcf8023b59be14a938f50a595329aaece2
tawshif <anikphp@gmail.com>
2017-03-16 09:02:21 +0600

    changes

--- app/index.js
+++ app/index.js
@@ -1,7 +1,24 @@
-import {fello, total} from './fello.js';
-import multiply  from './math.js';
+import Animal from './Animal.js';
+
+let king = new Animal("Mufasa", 3.4);
+
+console.log(king);
+
+king.hello();
+
+class Lion extends Animal{
+	constructor(name, height, color){
+		super (name, height);
+		this.color = color;
+	}
+
+	hello(){
+		console.log(` Hi i am ${this.name} from Pride Rock!`);
+	}
+}
 
 
-console.log(fello, total);
+let son = new Lion("Simba", 2, "golder");
 
-console.log(multiply(5,6));
+console.log(son);
+son.hello();
